3a80c960a0f982a59f47c1eca01dcd474d61e98b
GOKUL SONA <142295118+gokulsona@users.noreply.github.com>
2024-03-29 23:40:16 +0530

    Update matrix.java

--- JAVA/CO1/matrix.java
+++ JAVA/CO1/matrix.java
@@ -8,64 +8,39 @@ step 5 : Print the proper result
 Step 6 : End
 //Program
 import java.util.Scanner;
+class Main
+{
+public static void main(String args[])
+{
+int m, n, i, j;
+Scanner in = new Scanner(System.in);
+System.out.println("Enter the number of rows andcolumns of matrix : ");
+m = in.nextInt();
+n = in.nextInt();
+int first[][] = new int[m][n];
+int second[][] = new int[m][n];
+int sum[][] = new int[m][n];
+System.out.println("Enter the elements of first matrix : ");
+for (i = 0; i <m; i++)
+for (j = 0; j < n; j++)
+first[i][j] = in.nextInt();
+System.out.println("Enter the elements of second matrix : ");
+for (i = 0 ; i< m; i++)
 
-public class MatrixAddition {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Enter number of rows: ");
-        int rows = scanner.nextInt();
-
-        System.out.print("Enter number of columns: ");
-        int columns = scanner.nextInt();
-
-        int[][] matrix1 = inputMatrix(rows, columns, scanner, "first");
-        int[][] matrix2 = inputMatrix(rows, columns, scanner, "second");
-
-        int[][] sum = addMatrices(matrix1, matrix2);
-
-        displayMatrix(sum);
-
-        scanner.close();
-    }
-
-    // Method to input elements of a matrix
-    public static int[][] inputMatrix(int rows, int columns, Scanner scanner, String name) {
-        System.out.println("Enter elements of " + name + " matrix:");
-        int[][] matrix = new int[rows][columns];
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < columns; j++) {
-                matrix[i][j] = scanner.nextInt();
-            }
-        }
-        return matrix;
-    }
-
-    // Method to add two matrices
-    public static int[][] addMatrices(int[][] matrix1, int[][] matrix2) {
-        int rows = matrix1.length;
-        int columns = matrix1[0].length;
-        int[][] sum = new int[rows][columns];
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < columns; j++) {
-                sum[i][j] = matrix1[i][j] + matrix2[i][j];
-            }
-        }
-        return sum;
-    }
-
-    // Method to display a matrix
-    public static void displayMatrix(int[][] matrix) {
-        System.out.println("Sum of the matrices:");
-        for (int[] row : matrix) {
-            for (int element : row) {
-                System.out.print(element + " ");
-            }
-            System.out.println();
-        }
-    }
+for (j = 0 ; j < n; j++)
+second[i][j] = in.nextInt();
+for (i = 0; i<m; i++)
+for (j = 0; j <n; j++)
+sum[i][j] = first[i][j] + second[i][j]; 
+System.out.println("Sum of the matrices: ");
+for (i = 0; i< m; i++)
+{
+for (j = 0; j <n; j++)
+System.out.print(sum[i][j] + "\t");
+System.out.println();
 }
-
+}
+}
 //Output
 Enter number of rows: 2
 Enter number of columns: 2
